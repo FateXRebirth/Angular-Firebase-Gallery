70d6ce01386c704b51eef7cc085b89a76f3b9575
FateXRebirth <FateXRebirth@gmail.com>
2017-05-20 23:55:35 +0800

    Retrieve and Save Data

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './../../services/firebase.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  firstName: any;
+  lastName: any;
+  email: any;
+  password: any;
+  confirmation: any;
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+  ) { }
 
   ngOnInit() {
+    console.log("Navbar");
+    
+  }
+
+  signup() {
+    console.log("Submitting...");
+    
+    let user = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+    }
+
+
+    this.firebaseService.confirm(user.email); 
+
+    this.firebaseService.createUser(user);
+  }
+
+  login() {
+    let user = {
+      email: this.email,
+      password: this.password
+    }
+
   }
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -6,7 +6,77 @@ import * as firebase from 'firebase';
 
 @Injectable()
 export class FirebaseService {
+  users: FirebaseListObservable<any[]>;
+  folder: any;
+  ref: any;
 
-  constructor() { }
+  constructor(db: AngularFireDatabase) {
+    this.folder = "images";
+    this.users = db.list('/users') as FirebaseListObservable<User[]>;
+    this.ref = firebase.database().ref('/users');
+   }
 
+   createUser(user) {
+     this.users.push(user);
+   }
+
+   deleteUser(id) {
+     this.users.remove(id);
+   }
+
+   updateUser(id, data) {
+     this.users.update(id, data);
+   }
+
+   confirm(email) {
+     console.log("Confirming...");
+     this.ref.orderByKey().once("value").then(function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            // key will be "ada" the first time and "alan" the second time
+            var key = childSnapshot.key;
+            // childData will be the actual contents of the child
+            var childData = childSnapshot.val();
+            console.log("key->");
+            console.log(key);
+            console.log("val->");
+            console.log(childData);            
+          });
+        });     
+   }
 }
+
+
+interface User {
+  $key?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+}
+
+
+// export function checkIfUserExists(authData) {
+//   return db
+//     .child('users')
+//     .child(authData.uid)
+//     .once('value')
+//     .then(dataSnapshot => {
+//       return Promise.resolve({
+//         authData,
+//         userExists: dataSnapshot.exists(),
+//       });
+//     });
+// }
+
+// db.authWithOAuthPopup(provider)
+//     .then(checkIfUserExists)
+//     .then(({authData, userExists}) => {
+//       if (userExists) {
+//         // update user
+//       } else {
+//         // go create a user
+//       }
+//     })
+//     .catch(err => {
+//       console.warn('Error signing in.', err);
+//     });
